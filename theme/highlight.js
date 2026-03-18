509f78724504028ce9ffa6a471b8342f912e4594
ObserverOfTime <chronobserver@disroot.org>
2026-03-18 14:34:25 +0200

    Add CyberChef recipe highlighting

--- theme/highlight.js
+++ theme/highlight.js
@@ -459,7 +459,14 @@ contains:[e.TITLE_MODE,E],relevance:0},{match:/\(\)/},{className:"params",
 begin:/\(/,end:/\)/,excludeBegin:!0,excludeEnd:!0,keywords:n,relevance:0,
 contains:[g,i,e.C_BLOCK_COMMENT_MODE]
 },e.C_LINE_COMMENT_MODE,e.C_BLOCK_COMMENT_MODE]},b]}}})()
-;hljs.registerLanguage("csharp",e)})();/*! `dos` grammar compiled for Highlight.js 11.11.1 */
+;hljs.registerLanguage("csharp",e)})();/*! `cyberchef` grammar compiled for Highlight.js 11.11.1 */
+(()=>{var e=(()=>{"use strict";return e=>({name:"CyberChef Recipe",
+disableAutodetect:!0,contains:[{begin:[/\b[A-Za-z0-9_/]+/,/\(/],beginScope:{
+1:"title",2:"punctuation"},relevance:1},{className:"string",begin:/'/,end:/'/,
+contains:[{begin:/\\'/}]},{className:"number",begin:/\b\d+(\.\d+)?\b/},{
+className:"literal",begin:/\b(true|false)\b/},{className:"meta",
+begin:/\/(disabled|breakpoint)\b/},{className:"punctuation",begin:/[(),{}:]/}]})
+})();hljs.registerLanguage("cyberchef",e)})();/*! `dos` grammar compiled for Highlight.js 11.11.1 */
 (()=>{var e=(()=>{"use strict";return e=>{const r=e.COMMENT(/^\s*@?rem\b/,/$/,{
 relevance:10});return{name:"Batch file (DOS)",aliases:["bat","cmd"],
 case_insensitive:!0,illegal:/\/\*/,keywords:{
